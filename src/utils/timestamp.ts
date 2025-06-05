fdb938355337c4928d58cd4c2f352eee961d20b9
hassaneljebyly <hassaneljeby@gmail.com>
2025-06-05 22:51:55 +0100

    feat: implement time string to seconds function

---
+++ src/utils/timestamp.ts
@@ -0,0 +1,21 @@
+import type { FormDataType } from "../components/Note";
+
+export type SegmentBound = Pick<FormDataType, "end" | "start">;
+export type timeStringToSecondsType = Record<keyof SegmentBound, number>;
+
+export function timeStringToSeconds(
+  segmentBound: SegmentBound
+): timeStringToSecondsType {
+  const result: Record<string, number> = {};
+  for (const [bound, value] of Object.entries(segmentBound)) {
+    // start from back cause expected format is hh:mm:ss or mm:ss or ss
+    const valuesArray: number[] = value.split(":").map(Number);
+    // TODO config ts to remove "Property 'at' does not exist on type 'number[]'" error
+    const seconds = valuesArray.at(-1) || 0;
+    const minutes = valuesArray.at(-2) * 60 || 0;
+    const hours = valuesArray.at(-3) * 3600 || 0;
+    result[bound] = seconds + minutes + hours;
+  }
+
+  return result as timeStringToSecondsType;
+}
