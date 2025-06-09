93bd3adba3b4ef75fbc36b63e1cfcb9a63eff6c3
hassaneljebyly <hassaneljeby@gmail.com>
2025-06-09 21:35:35 +0100

    fix: timeStringToSecond undefined issue

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -1,11 +1,10 @@
 export function timeStringToSeconds(segmentBound: string) {
   let result = 0;
   const valuesArray: number[] = segmentBound.split(":").map(Number);
-  // TODO config ts to remove "Property 'at' does not exist on type 'number[]'" error
   // start from back cause expected format is hh:mm:ss or mm:ss or ss
   const seconds = valuesArray.at(-1) || 0;
-  const minutes = valuesArray.at(-2) * 60 || 0;
-  const hours = valuesArray.at(-3) * 3600 || 0;
+  const minutes = (valuesArray.at(-2) || 0) * 60;
+  const hours = (valuesArray.at(-3) || 0) * 3600;
   result = seconds + minutes + hours;
 
   return result;
