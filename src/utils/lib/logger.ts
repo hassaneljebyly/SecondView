787a9f9107f8aca60b96857a14fd38685c0bd624
hassaneljebyly <hassaneljeby@gmail.com>
2025-07-22 11:49:46 +0100

    feat: add stack trace tracker

--- src/utils/lib/logger.ts
+++ src/utils/lib/logger.ts
@@ -4,15 +4,20 @@ import { IS_DEV, LOGGER_LABEL, LOGGER_STYLES } from '../config/loggerConfig';
 
 export const logger = {
   debug: (...args: unknown[]) => {
-    if (IS_DEV) console.log(LOGGER_LABEL, LOGGER_STYLES['debug'], ...args);
+    if (IS_DEV) console.log(LOGGER_LABEL, LOGGER_STYLES['debug'], ...args, source());
   },
   info: (...args: unknown[]) => {
-    console.info(LOGGER_LABEL, LOGGER_STYLES['info'], ...args);
+    console.info(LOGGER_LABEL, LOGGER_STYLES['info'], ...args, source());
   },
   warn: (...args: unknown[]) => {
-    console.warn(LOGGER_LABEL, LOGGER_STYLES['warn'], ...args);
+    console.warn(LOGGER_LABEL, LOGGER_STYLES['warn'], ...args, source());
   },
   error: (...args: unknown[]) => {
-    console.error(LOGGER_LABEL, LOGGER_STYLES['error'], ...args);
+    console.error(LOGGER_LABEL, LOGGER_STYLES['error'], ...args, source());
   },
 };
+
+function source() {
+  const location = `${new Error().stack?.split('\n')[3]}`.match(/(\(.+\))/);
+  return location ? `\n at ${location[0]}` : '';
+}
