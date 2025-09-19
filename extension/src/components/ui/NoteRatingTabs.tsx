3ddd3ce69fe73f07c9d8c5a288dd379642ee41c8
hassaneljebyly <hassaneljeby@gmail.com>
2025-09-19 23:33:56 +0100

    feat: add basic styles & structure for note rating

--- extension/src/components/ui/NoteRatingTabs.tsx
+++ extension/src/components/ui/NoteRatingTabs.tsx
@@ -1,3 +1,51 @@
+import { RATINGS_CHECKBOXES_TABS } from '@/utils/config/notRatingConfig';
+
+import Button from './Button';
+
 export default function NoteRatingTabs() {
-  return <div className='.note-rating-tabs'>NoteRatingTabs</div>;
+  return (
+    <div className='note-rating' aria-hidden='true'>
+      <form className='note-rating__form'>
+        <fieldset className='note-rating__fieldset'>
+          <legend id='sv-tablist-1' className='note-rating__legend sv-divider sv-divider--bottom'>
+            How Accurate was this note?
+          </legend>
+
+          <div className='note-rating__tabs sv-divider sv-divider--bottom' role='tablist'>
+            <Button text='Accurate' shape='rounded' theme='light' role='tab' />
+            <Button text='Inaccurate' shape='rounded' theme='dark' role='tab' />
+          </div>
+
+          <div className='note-rating__reasons sv-divider sv-divider--bottom'>
+            <h3 className='note-rating__checkbox-heading'>Why was this note helpful?</h3>
+            {RATINGS_CHECKBOXES_TABS['accurate'].map(({ value, label }) => {
+              return (
+                <label key={value} className='note-rating__checkbox-label' htmlFor={value}>
+                  {label}
+                  <input
+                    id={value}
+                    type='checkbox'
+                    name='accurate'
+                    value={value}
+                    className='sv-checkbox'
+                  />
+                </label>
+              );
+            })}
+          </div>
+
+          <div className='note-rating__action'>
+            <Button text='Cancel' shape='pill' role='tab' />
+            <Button
+              text='Submit'
+              shape='pill'
+              theme='blue'
+              role='tab'
+              icon={{ variant: 'upload' }}
+            />
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
