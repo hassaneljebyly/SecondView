2996984f39470b008f5f31e6916d8a9caefd5135
hassaneljebyly <hassaneljeby@gmail.com>
2025-09-23 21:26:17 +0100

    nameless commit

--- extension/src/utils/config/injectTasksConfig.tsx
+++ extension/src/utils/config/injectTasksConfig.tsx
@@ -1,17 +1,12 @@
 import { FormToggleButtonTask } from '@/components/content/FormToggleButton/injectTask';
+import { NoteDisplayQueueTask } from '@/components/content/NoteDisplayQueue/injectTask';
 import { NoteFormTask } from '@/components/content/NoteForm/injectTask';
 import { NoteSegmentsBarTask } from '@/components/content/NoteSegmentsBar/injectTask';
 import type { InjectTask } from '@/types';
 
-export const INJECT_TASK: InjectTask[] = [FormToggleButtonTask, NoteFormTask, NoteSegmentsBarTask];
-
-// {
-//   domTargetSelector: '#player-container:has(#ytd-player)',
-//   rootWrapperId: withPrefix('note-popup-root'),
-//   componentId: withPrefix('note-popup'),
-//   component: (
-//     <StrictModeWrapper>
-//       <NoteQueuePopUp />
-//     </StrictModeWrapper>
-//   ),
-// },
+export const INJECT_TASK: InjectTask[] = [
+  FormToggleButtonTask,
+  NoteFormTask,
+  NoteSegmentsBarTask,
+  NoteDisplayQueueTask,
+];
