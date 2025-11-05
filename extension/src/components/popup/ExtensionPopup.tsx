ba1fcccbf38775297d4cbf1c5fb6cc83b6525444
hassaneljebyly <hassaneljeby@gmail.com>
2025-11-05 21:01:59 +0100

    refactor: improve ppopup UI/UX

--- extension/src/components/popup/ExtensionPopup.tsx
+++ extension/src/components/popup/ExtensionPopup.tsx
@@ -1,17 +1,5 @@
-import ContributionStats from './ContributionStats';
-import QuickLinks from './QuickLinks';
-import UserCard from './UserCard';
 import Button from '../ui/Button';
 
 export default function MainPopup() {
-  return (
-    <div className='popup'>
-      <UserCard />
-      <ContributionStats />
-      <QuickLinks />
-      <footer className='popup__footer footer'>
-        <Button text='settings' icon={{ variant: 'setting' }} shape='rounded' />
-      </footer>
-    </div>
-  );
+  return <div className='popup'>hello world</div>;
 }
