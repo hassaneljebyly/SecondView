66e835e02c5debcfb24b58f09ddde65f35eb214a
hassaneljebyly <hassaneljeby@gmail.com>
2025-11-17 19:55:17 +0100

    feat: add icons

--- extension/src/components/ui/Icon.tsx
+++ extension/src/components/ui/Icon.tsx
@@ -12,7 +12,8 @@ export type IconProps = {
     | 'paste'
     | 'edit'
     | 'login'
-    | 'shield';
+    | 'shield'
+    | 'fileDownload';
   theme?: 'dark' | 'light';
   size?: 'xs' | 'sm' | 'md' | 'lg';
   animation?: string;
@@ -201,6 +202,19 @@ export default function Icon({ variant, theme, size, animation }: IconProps) {
           aria-hidden='true'
         >
           <path d='M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z' />
+        </svg>
+      );
+    case 'fileDownload':
+      return (
+        <svg
+          className={`${iconClass}`}
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 -960 960 960'
+          width='24px'
+          height='24px'
+          aria-hidden='true'
+        >
+          <path d='m720-120 160-160-56-56-64 64v-167h-80v167l-64-64-56 56 160 160ZM560 0v-80h320V0H560ZM240-160q-33 0-56.5-23.5T160-240v-560q0-33 23.5-56.5T240-880h280l240 240v121h-80v-81H480v-200H240v560h240v80H240Zm0-80v-560 560Z' />
         </svg>
       );
   }
