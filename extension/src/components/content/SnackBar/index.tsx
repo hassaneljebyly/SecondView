28f6f3c2c6ecc91216671e93d7601d46938b7a5e
hassaneljebyly <hassaneljeby@gmail.com>
2026-02-09 14:10:30 +0100

    feat: add snackbar initial structure and styles

--- extension/src/components/content/SnackBar/index.tsx
+++ extension/src/components/content/SnackBar/index.tsx
@@ -1,8 +1,31 @@
+// reference https://m3.material.io/components/snackbar/specs
+
+import Button from '@/components/ui/Button';
+
 export const snackBarId = 'sv-snackbar';
+
 export default function SnackBar() {
+  const initIalClass = 'sv-animation-pop-in-initial';
+  const showSnackBarClass = 'sv-animation-pop-in';
   return (
-    <div id={snackBarId} className='sv-snackbar'>
-      SnackBar
+    <div id={snackBarId} className={`sv-snackbar ${initIalClass}`}>
+      <p className='sv-snackbar__text'>Single line snackbar with action</p>
+      <div className='sv-snackbar__controls'>
+        <div className='sv-snackbar__action'>
+          <Button text='Action' noDarkMode theme='light' shape='rounded' />
+        </div>
+        <div className='sv-snackbar__close'>
+          <Button text='close notification' icon={{ variant: 'cancel', size: 'xs' }} iconOnly />
+        </div>
+      </div>
     </div>
   );
 }
+
+// Container
+//
+// Icon (optional close affordance)
+//
+// Action (optional)
+//
+// Supporting text
